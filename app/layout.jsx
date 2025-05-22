b091fc72e8c18b7eacd682dffc271a50d717eb3e
Wealiums <Brodie.weal@education.nsw.gov.au>
2025-05-22 12:49:49 +1000

    Initial
    Initital

---
+++ app/layout.jsx
@@ -0,0 +1,25 @@
+import { Inter } from "next/font/google";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import "@/assets/styles/globals.css";
+
+const inter = Inter({subsets: ["latin"],});
+
+export const metadata = {
+  title: "CDH Booking System",
+  description: "Book a phone or in person meeting with a CDH accountent",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={ inter.className}>
+        <Header />
+        <main className='mx-auto.max-w-7xl px-4 py-6 sm:px-6 lg:px-8'>
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import RoomCard from '@/components/RoomCard';
+import rooms from '@/data/rooms.json';
+
+export default function Home() {
+  return (
+    <>
+      
+      {rooms.length > 0 ? (
+        rooms.map((room) => <RoomCard room={room} />)
+      ) : (<p>No rooms available at the moment</p>) }
+    </>
+  );
+}
