33ed1b71cc48131858d7b8c14d1192ae593c7682
Wealiums <Brodie.weal@education.nsw.gov.au>
2025-05-26 10:49:57 +1000

    continue creating booking page

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,5 @@
 import Heading from '@/components/Heading';
 import RoomCard from '@/components/RoomCard';
-import Heading from '@/components/Heading';
 import rooms from '@/data/rooms.json';
 
 export default function Home() {
