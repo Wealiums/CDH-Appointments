e47d78ab8c1e0b479cfb9dabbcb579f80f0423b2
Wealiums <Brodie.weal@education.nsw.gov.au>
2025-06-08 21:02:36 +1000

    login in and log out

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,26 @@
+'use client';
+import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 import Link from 'next/link';
 import logo from '@/assets/images/logo.svg';
 import { FaUser, FaSignInAlt, FaSignOutAlt, FaBuilding } from 'react-icons/fa';
+import destroySession from '@/app/actions/destroySession';
+import {toast} from 'react-toastify';
 
 const Header = () => {
+    const router = useRouter();
+
+    const handleLogout = async () => {
+        const { success, error } = await destroySession();
+
+        if (success) {
+            router.push('/login');
+        } else {
+            toast.error(error);
+        }
+    }
+
+
     return (
         <header className="bg-gray-100">
             <nav className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -60,12 +77,9 @@ const Header = () => {
                         <FaBuilding className='inline mr-1' /> My Rooms
                     </Link>
                         {/* <!-- Logged In Only --> */}
-                    <Link
-                        href="/login"
-                        className="mx-3 text-gray-800 hover:text-gray-600"
-                    >
+                    <button onClick={handleLogout} className="mx-3 text-gray-800 hover:text-gray-600">
                         <FaSignOutAlt className='inline mr-1' /> Sign Out
-                    </Link>
+                    </button>
                         {/* <!-- Logged In Only --> */}
                     </div>
                 </div>
