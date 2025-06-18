37fdd0488ab0ef3333b748d8e4860965975cb022
Wealiums <Brodie.weal@education.nsw.gov.au>
2025-06-19 01:39:07 +1000

    authorisation
    getting rid of certain buttons when not logged in etc

--- app/actions/checkAuth.js
+++ app/actions/checkAuth.js
@@ -1,9 +1,9 @@
-'use client';
+'use server';
 import { createSessionClient } from "@/config/appwrite";
 import { cookies } from "next/headers";
 
 async function checkAuth() {
-    const sessionCookie = cookies().get
+    const sessionCookie = cookies().get('appwrite-session');
 
     if (!sessionCookie) {
         return {
@@ -12,9 +12,21 @@ async function checkAuth() {
     }
 
     try {
-        const { account }
+        const { account } = await createSessionClient(sessionCookie.value);
+        const user = await account.get();
+
+        return {
+            isAuthenticated: true,
+            user: {
+                id: user.$id,
+                name: user.name,
+                email: user.email,
+            },
+        };
     } catch (error) {
-        
+      return {
+        isAuthenticated: false,  
+      };    
     }
 }
 
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,5 @@
 'use client';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -6,19 +7,31 @@ import logo from '@/assets/images/logo.svg';
 import { FaUser, FaSignInAlt, FaSignOutAlt, FaBuilding } from 'react-icons/fa';
 import destroySession from '@/app/actions/destroySession';
 import {toast} from 'react-toastify';
+import checkAuth from '@/app/actions/checkAuth';
 
 const Header = () => {
     const router = useRouter();
 
+    const [isAuthenticated, setIsAuthenticated] = useState(null);
+
+    useEffect(() => {
+        const fetchAuthStatus = async () => { 
+            const result = await checkAuth();
+            setIsAuthenticated(result.isAuthenticated);
+        } 
+        
+        fetchAuthStatus();
+    }, []);
+
     const handleLogout = async () => {
-        const { success, error } = await destroySession();
+        const { sucess, error } = await destroySession();
 
         if (success) {
             router.push('/login');
         } else {
             toast.error(error);
         }
-    }
+    };
 
 
     return (
@@ -38,20 +51,24 @@ const Header = () => {
                         Rooms
                         </Link>
                         {/* <!-- Logged In Only --> */}
-                        <Link
-                        href="/bookings"
-                        className="rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white"
+                        { isAuthenticated && (
+                            <>
+                            <Link
+                            href="/bookings"
+                            className="rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white"
                         >
-                        Bookings
+                            Bookings
                         </Link>
-                        {/* <!-- Logged In Only --> */}
                         <Link
-                        href="/rooms/add"
-                        className="rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white"
+                            href="/rooms/add"
+                            className="rounded-md px-3 py-2 text-sm font-medium text-gray-800 hover:bg-gray-700 hover:text-white"
                         >
                         Add Room
                         </Link>
-                        {/* <!-- Logged In Only --> */}
+                            </>
+                        ) }
+                        
+                        
                     </div>
                     </div>
                 </div>
@@ -59,19 +76,24 @@ const Header = () => {
                 <div className="ml-auto">
                     <div className="ml-4 flex items-center md:ml-6">
                     {/* <!-- Logged Out Only --> */}
-                    <Link
+                    { !isAuthenticated && (
+                        <>
+                        <Link
                         href="/login"
                         className="mr-3 text-gray-800 hover:text-gray-600"
                     >
                         <FaSignInAlt className='inline mr-1' /> Login
                     </Link>
-                        {/* <!-- Logged In Only --> */}
+                        
                     <Link
                         href="/register"
                         className="mr-3 text-gray-800 hover:text-gray-600"
                     >
                         <FaUser className='inline mr-1' /> Register
                     </Link>
+                        </>
+                    ) }
+                    
                         {/* <!-- Logged In Only --> */}
                     <Link href="/rooms/my">
                         <FaBuilding className='inline mr-1' /> My Rooms
